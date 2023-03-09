f3ff6de4b40aeac43caf64f5fa767402bb139691
©¤╬¤/\/\\//\/\C|X¤╬¤© <mvmclx@gmail.com>
2023-03-09 04:25:28 -0600

    re-structure

---
+++ Notifications Challenge/public/images/index.js
@@ -0,0 +1,25 @@
+const Angela_Gray = require('./avatar-angela-gray.webp');
+const Anna_Kim = require('./avatar-anna-kim.webp');
+const Jacob_Thompson = require('./avatar-jacob-thompson.webp');
+const Kimberly_Smith = require('./avatar-kimberly-smith.webp');
+const Mark_Webber = require('./avatar-mark-webber.webp');
+const Nathan_Peterson = require('./avatar-nathan-peterson.webp');
+const Rizky_Hasanuddin = require('./avatar-rizky-hasanuddin.webp');
+const imageChess = require('./image-chess.webp');
+
+
+
+module.exports = {
+    Angela_Gray,
+    Anna_Kim,
+    Jacob_Thompson,
+    Kimberly_Smith,
+    Mark_Webber,
+    Nathan_Peterson,
+    Rizky_Hasanuddin,
+    imageChess,
+  };
+
+
+
+
---
+++ Notifications Challenge/src/components/api/index.js
@@ -0,0 +1,90 @@
+module.exports = {
+    "notifications": [
+        {
+            "id": "shchDJrYY",
+            "notification": "reacted to your recent post",
+            "type": "reaction",
+            "timestamp": "3 min ago",
+            "author": {
+                "name": "Mark Webber",
+                "img": "avatar-mark-weber.webp"
+            },
+            "link": "My first tournament today!",
+            "read": "false"
+        },
+        {
+            "id": "jcvfJghkY",
+            "notification": "followed you",
+            "type": "follow",
+            "timestamp": "5 min ago",
+            "author": {
+                "name": "Angela Gray",
+                "img": "avatar-angela-gray.webp"
+            },
+            "link": "",
+            "read": "false"
+        },
+        {
+            "id": "gHJGjhrEe",
+            "notification": "has joined your group",
+            "type": "group",
+            "timestamp": "1 day ago",
+            "author": {
+                "name": "Jacob Thompson",
+                "img": "avatar-jacob-thompson.webp"
+            },
+            "link": "Chess Club",
+            "read": "false"
+        },
+        {
+            "id": "jdjdjJHgY",
+            "notification": "sent you a private message",
+            "type": "message",
+            "timestamp": "5 days ago",
+            "author": {
+                "name": "Rizky Hasanuddin",
+                "img": "avatar-rizky-hasanuddin.webp",
+                "message": "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game."
+            },
+            "link": "",
+            "read": "false"
+        },
+        {
+            "id": "gshtdDYrT",
+            "notification": "commented on your picture",
+            "type": "comment",
+            "timestamp": "1 week ago",
+            "author": {
+                "name": "Kimberly Smith",
+                "img": "avatar-kimberly-smith.webp",
+                "picture": ""
+            },
+            "link": "",
+            "read": "false"
+        },
+        {
+            "id": "djtrJHgTj",
+            "notification": "reacted to your recent post",
+            "type": "reaction",
+            "timestamp": "2 weeks ago",
+            "author": {
+                "name": "Nathan Peterson",
+                "img": "avatar-nathan-peterson.webp"
+            },
+            "link": "5 end-game strategies to increase your win rate",
+            "read": "false"
+        },
+        {
+            "id": "dhrtyJHqg",
+            "notification": "left the group",
+            "type": "group",
+            "timestamp": "2 weeks ago",
+            "author": {
+                "name": "Anna Kim",
+                "img": "avatar-anna-kim.webp"
+            },
+            "link": "Chess Club",
+            "read": "false"
+        }
+    ]
+}
