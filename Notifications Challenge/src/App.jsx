dbeb3437033e81c162dd7afbf94a77d1d1346da2
©¤╬¤/\/\\//\/\C|X¤╬¤© <mvmclx@gmail.com>
2023-03-09 23:51:07 -0600

    pretty well there

--- Notifications Challenge/src/App.jsx
+++ Notifications Challenge/src/App.jsx
@@ -2,8 +2,22 @@
  import React from "react";
  import Notifications from "./components/Notifications";
  
- 
+import data from './components/mydata.json'
 const App = () => {
+
+  
+
+// const read = 
+
+//  const notificationLineItem = document.getElementById('notificationrow')
+  
+//     // const unreadNotifications = data.filter(notification => !notification.read);
+//     // const readNotifications = data.filter(notification => notification.read);
+// if(data.notifications)
+//   let notifcationId = notification.id
+//   console.log(notifcationId, "IDIDIDID", notification.author)
+
+// }
   return (
     <div>
       <Notifications />
--- Notifications Challenge/src/components/Notifications.jsx
+++ Notifications Challenge/src/components/Notifications.jsx
@@ -1,72 +1,119 @@
 
-
-import React from 'react';
-import data from './data.json';
-
+import React,{useState,useEffect} from 'react'
+import data from './data.json'
 const Notifications = () => {
+
+
   const [notifications, setNotifications] = React.useState(data.notifications);
-  const [readCount, setReadCount] = React.useState(
-    notifications.filter((notification) => notification.read === false).length
-  );
+  const [readCount, setReadCount] = useState(notifications.filter(notification => notification.read).length);
+console.log(readCount)
 
+  useEffect(() => {
+
+    const readCount = notifications.filter(notification => notification.read).length;
+    setReadCount(readCount);
+  }, [notifications]);
   const handleHover = (notification) => {
-    notification.read = true;
-    setNotifications([...notifications]);
-    setReadCount(readCount > 0 ? readCount - 1 : 0);
-  };
+    if (notification.read) {
+      alert(`Notification read ${notification.timestamp}.`);
+    } else {
+      alert(
+        `Unread notification from ${JSON.stringify(
+          notification.author.name
+        )}\n\n \b\b\b\bMessage\b\b\b\b\n\n${JSON.stringify(
+          notification.notification
+        )}`
+      );setReadCount(readCount -1)
+      notification.read = true;
+      setNotifications([...notifications]);
+console.log(readCount)
 
-  const handleFocus = (notification) => {
-    notification.read = true;
-    setNotifications([...notifications]);
-    setReadCount(readCount > 0 ? readCount - 1 : 0);
-  };
 
-  const handleMarkAllRead = () => {
+
+setReadCount(notifications.filter(notification => notification.read).length);
+console.log(readCount)
+    }
+  };
+  const handleMarkAllRead = (notification) => {
     notifications.map((notification) => (notification.read = true));
+
     setNotifications([...notifications]);
     setReadCount(0);
   };
 
-  const handleMarkAllUnRead = () => {
+  const handleMarkAllUnRead = (notification) => {
     notifications.map((notification) => (notification.read = false));
+
     setNotifications([...notifications]);
     setReadCount(notifications.length);
   };
 
-
   return (
     <div>
-      <NotificationHeader readCount={readCount} handleMarkAllRead={handleMarkAllRead} handleMarkAllUnRead={handleMarkAllUnRead}/>
+      <NotificationHeader
+        readCount={readCount}
+        handleMarkAllRead={handleMarkAllRead}
+        handleMarkAllUnRead={handleMarkAllUnRead}
+      />
       <NotificationList
         notifications={notifications}
         onHover={handleHover}
-        onFocus={handleFocus}
       />
     </div>
   );
 };
 
-const NotificationHeader = ({ readCount, handleMarkAllRead, handleMarkAllUnRead }) => {
+const NotificationHeader = ({
+  readCount,
+  handleMarkAllRead,
+  handleMarkAllUnRead,
+  fromWho
+}) => {
   return (
-    <div className='header-wrapper'>
-      <div className='element1'>
-      <h2>Notifications <span className='notification-icon'>({readCount})</span></h2></div>
-<div className='header-select-all'>
-     {readCount === 0 ? <button className='element2 no-button' onClick={handleMarkAllUnRead}>Mark All UnRead</button> : <button className='element2 no-button' onClick={handleMarkAllRead}>Mark All Read</button> }
-      </div>  </div>
+    <div className="header-wrapper">
+      <div className="element1">
+        <h2>
+          Notifications{" "}
+          <span className="notification-icon">({readCount})</span>
+          {fromWho}
+        </h2>
+      </div>
+      <div className="header-select-all">
+        {readCount === 0 ? (
+          <button className="element2 no-button" onClick={handleMarkAllRead}>
+            Mark All UnRead
+          </button>
+        ) : (
+          <button className="element2 no-button" onClick={handleMarkAllUnRead}>
+            Mark All Read
+          </button>
+        )}
+      </div>
+    </div>
   );
 };
 
-const NotificationList = ({ notifications, onHover, onFocus }) => {
+const NotificationList = ({ notifications, onHover, onFocus, notification }) => {
   return (
-    <div>
+    <div
+      onMouseEnter={() => {
+        if (notification.read) {
+          onHover(notification);
+        }
+      }}
+      onFocus={() => {
+        if (notification.read) {
+          onFocus(notification);
+        }
+      }}
+      className="notifications col-one"
+    >
       {notifications.map((notification) => (
         <Notification
           key={notification.id}
           notification={notification}
           onHover={onHover}
-          onFocus={onFocus}
-          className="notification"
+          read={notification.read}
         />
       ))}
     </div>
@@ -74,27 +121,28 @@ const NotificationList = ({ notifications, onHover, onFocus }) => {
 };
 
 const Notification = ({ notification, onHover, onFocus }) => {
-
-  const style = { backgroundColor:notification.read ?  '#FFFFFF' :'#F0F0F0'}
   return (
+    <div onMouseEnter={() => onHover(notification)} onFocus={() => onFocus(notification)}>
+      <div className="col-one">
+        {notification.author.img && (
+          <img src={notification.author.img} alt="notification" />
+        )}
+      </div>
 
-    <div style={style} onMouseEnter={() => onHover(notification)} onFocus={() => onFocus(notification)} className="notifications col-one">
-    <div className='col-one'>
-       {notification.author.img && <img src={notification.author.img}/>}
-       </div>
-   
-    
-    <div className='col-two'>
-  <p className='note-item'>{notification.author.name}</p>
-      <h2 className="note-item name">{notification.notification}</h2>
-         {notification.link && <a className="note-item bold" href={notification.link}>{notification.link}</a>}
-            <p className='note-item'>{notification.timestamp}</p>
-    
-     </div>
-
+      <div className="col-two">
+        <p className="note-item">{notification.author.name}</p>
+        <h2 className="note-item name">{notification.notification}</h2>
+        {notification.link && (
+          <a className="note-item bold" href={notification.link}>
+            {notification.link}
+          </a>
+        )}
+        <p className="note-item">
+          {notification.timestamp || notification.readAtTimestamp}
+        </p>
+      </div>
     </div>
   );
 };
 
 export default Notifications;
-
